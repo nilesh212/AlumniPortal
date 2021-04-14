dc480df76c476fdb2594c24e2be6f3fbda969c0e
Nilesh Mare <67241872+nilesh212@users.noreply.github.com>
2021-04-14 20:30:23 +0530

    Delete PortalAlumni.old directory

--- PortalAlumni.old/app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from app.models import SignUpModel,PostModel,EventModel
-# Register your models here.
-admin.site.register(SignUpModel)
-admin.site.register(PostModel)
-admin.site.register(EventModel)
